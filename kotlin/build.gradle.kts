ade62c811d53e7aff2eae622e2949920aee9131a
Aleksandr Krasikov <kollapze@gmail.com>
2026-09-01 13:48:38 +0200

    Dependabot updates

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -20,3 +20,31 @@ allprojects {
         }
     }
 }
+
+// Pin patched transitive npm versions in the Kotlin/JS yarn.lock.
+// Dual-major packages (brace-expansion, minimatch) are updated in yarn.lock
+// within their existing ranges rather than forced to a single version here.
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().apply {
+        resolution("webpack", "5.104.1")
+        resolution("lodash", "4.18.0")
+        resolution("decode-uri-component", "0.5.0")
+        resolution("engine.io", "6.6.7")
+        resolution("nanoid", "3.3.18")
+        resolution("js-yaml", "4.3.1")
+        resolution("socket.io-parser", "4.2.7")
+        resolution("body-parser", "1.20.6")
+        resolution("ws", "8.21.0")
+        resolution("tmp", "0.2.6")
+        resolution("serialize-javascript", "7.0.5")
+        resolution("picomatch", "2.3.2")
+        resolution("flatted", "3.4.2")
+        resolution("qs", "6.14.2")
+        resolution("diff", "5.2.2")
+        resolution("cookie", "0.7.2")
+        resolution("cross-spawn", "7.0.6")
+        resolution("braces", "3.0.3")
+        resolution("follow-redirects", "1.16.0")
+        resolution("rollup", "2.80.0")
+    }
+}
